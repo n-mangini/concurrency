a34e3ddd7cc168f02b0e33b6a120f9be6514dcf6
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2026-03-10 00:20:23 +0000

    calculate PI

---
+++ tp/tp1/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let pi = calculate_pi(100);
+    println!("{}", pi);
+}
+
+fn calculate_pi(i: u64) -> f64 {
+    let mut suma = 0.0;
+    for k in 0..i {
+        let denominador = (2 * k + 1) as f64;
+        let termino = 1.0 / denominador;
+        if k % 2 == 0 {
+            suma += termino;
+        } else {
+            suma -= termino;
+        }
+    }
+    4.0 * suma
+}
