b289217d3a13fd66977fa6bf2d149459ce622a21
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2026-03-10 01:38:04 +0000

    pi number as http response

--- tp/tp1/src/main.rs
+++ tp/tp1/src/main.rs
@@ -1,4 +1,5 @@
 use std::io::Read;
+use std::io::Write;
 use std::net::{TcpListener, TcpStream};
 
 fn handle_client(mut stream: TcpStream) {
@@ -13,7 +14,14 @@ fn handle_client(mut stream: TcpStream) {
     let path = req_parts[1];
     let iterations: &str = path.split('/').last().unwrap();
 
-    println!("{}", calculate_pi(iterations.parse().unwrap()));
+    let response = format!(
+        "HTTP/1.1 200 OK\r\n\r\nValor de pi: {}\n",
+        calculate_pi(iterations.parse().unwrap())
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+
+    //println!("{}", calculate_pi(iterations.parse().unwrap()));
 }
 
 fn main() -> std::io::Result<()> {
